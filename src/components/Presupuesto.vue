f8480597a8ba346dd37d6b9ed357637cf3b1cce2
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-06-14 21:37:16 -0500

    Agregando estilos a App.vue

---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+  <form>
+    <div class="campo">
+      <label for="nuevo-presupuesto">Definir Presupuesto</label>
+      <input
+        type="text"
+        id="nuevo-presupuesto"
+        class="nuevo-presupuesto"
+        placeholder="Añadir Presupuesto"
+      />
+    </div>
+    <input type="submit" value="Definir Presupuesto" />
+  </form>
+</template>
+
+<style scoped></style>
