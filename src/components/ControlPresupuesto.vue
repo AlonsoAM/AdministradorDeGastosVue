37d6c60b9e706fd9237806e03c17a877b6a9da48
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-07-13 22:25:02 -0500

    Graficar el avance del gasto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,5 +1,6 @@
 <script setup>
-import imagen from "../assets/img/grafico.jpg";
+import CircleProgress from "vue3-circle-progress";
+import "vue3-circle-progress/dist/circle-progress.css";
 import { formatearCantidad } from "../helpers";
 
 defineEmits(["reset-app"]);
@@ -22,7 +23,14 @@ const props = defineProps({
 <template>
   <div class="dos-columnas">
     <div class="contenedor-grafico">
-      <img :src="imagen" alt="imagen loader" />
+      <CircleProgress
+        :percent="50"
+        :size="250"
+        :border-width="30"
+        :border-bg-width="30"
+        fill-color="#3b82f6"
+        empty-color="#e1e1e1"
+      />
     </div>
     <div class="contenedor-presupuesto">
       <button type="button" class="reset-app" @click="$emit('reset-app')">
